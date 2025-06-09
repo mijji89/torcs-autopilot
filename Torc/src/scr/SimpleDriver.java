caabbd69b615ed9dcb1b44676765b758da4d9554
Benedetta <b.dellorusso@studenti.unisa.it>
2025-06-09 18:28:20 +0200

    normalizedataset si e no

--- Torc/src/scr/SimpleDriver.java
+++ Torc/src/scr/SimpleDriver.java
@@ -8,7 +8,7 @@ import javax.swing.SwingUtilities;
 
 public class SimpleDriver extends Controller {
 	//Flag booleana che mi permette di leggere o meno i valori di tastiera
-	private boolean training = true;   
+	private boolean training = false;   
 	private char pressed;  
 	
 	/* Costanti di cambio marcia */
@@ -55,7 +55,7 @@ public class SimpleDriver extends Controller {
 	// current clutch
 	private float clutch = 0;
 
-	File file = new File("datasetMic.csv");
+	File file = new File("dataset.csv");
 
 	public SimpleDriver(){
 		if (training & !file.exists()){
